42a71befd9bbe9732c900cadee234e097ae9dec7
niranjanaj628 <niranjanaj77@gamil.com>
2025-06-13 22:08:40 +0530

    added day 33 files

---
+++ projects/Day-33/issoverhead/main.py
@@ -0,0 +1,59 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+   
+MY_LAT = 19.07 # Your latitude
+MY_LONG = 72.87 # Your longitude
+
+def check():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+
+    if (MY_LAT - 5 <= iss_latitude <= MY_LAT+5) and (MY_LONG - 5 <= iss_longitude <= MY_LONG):
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+    
+    if time_now >=sunset or time_now <=sunrise:
+        return True
+    
+
+
+# Then send me an email to tell me to look up.
+my_email = '-'
+password= '-'
+to_email = "-"
+
+# run the code every 60 seconds.
+while True:
+    time.sleep(60)
+    if check() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(user=my_email, password= password)
+        connection.sendmail(from_addr=my_email, to_addrs=to_email, msg= "Subject: Look up at the sky! \n\n ISS is flying over you !")
+        
+        
+
+
+
--- projects/Day-33/main.py
+++ projects/Day-33/main.py
@@ -1,12 +1,35 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="https://open-notify.org/Open-Notify-API/ISS-Location-Now/")
+# response = requests.get(url="https://open-notify.org/Open-Notify-API/ISS-Location-Now/")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data['iss_position']['longitude']
+# latitude = data['iss_position']['latitude']
+
+# iss_position = (latitude, longitude)
+# print(iss_position)
+
+my_lat = 19.07
+my_lng = 72.87
+ 
+parameteres = {"lat" : my_lat, 
+               "lng" : my_lng,
+               "formatted" : 0
+              }
+
+response = requests.get("https://api.sunrise-sunset.org/json" , params= parameteres)
 response.raise_for_status()
 
 data = response.json()
+# print(data)
 
-longitude = data['iss_position']['longitude']
-latitude = data['iss_position']['latitude']
+sunrise = data['results']['sunrise'].split('T')[1].split(":")[0]
+sunset = data['results']['sunset'].split('T')[1].split(":")[0]
 
-iss_position = (latitude, longitude)
-print(iss_position)
+print('sunrise:' ,sunrise)
+print('sunset:' ,sunset)
+time_now = datetime.now()
+print(f'Time now: {time_now.hour}')
